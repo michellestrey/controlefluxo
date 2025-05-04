e5f3331e94fd6d8602916460cfa984e0dfe386d0
Michelle <micastrey@gmail.com>
2025-05-04 19:14:59 -0300

    First Commit

---
+++ desafioDioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package desafioDioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception{
+	
+	
+	public ParametrosInvalidosException(String mensagem) {
+		super(mensagem);
+		
+		
+	}
+		
+}
